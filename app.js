9ea7404cf8cbf3eda238c6c9c3f4f750e27914e9
Gary Peabody <aikarvin99@gmail.com>
2020-03-25 21:30:38 +0800

    ChatterProject with some CSS

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const fs = require("fs");
+const autoIncrement = require("mongoose-auto-increment");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/ChatterProject", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const ChitterSchema = new mongoose.Schema({
+  userName: {type:String, minlength: 1}, 
+  content: {type: String, minlength: 1, maxlength:280}
+});
+
+autoIncrement.initialize(mongoose.connection);
+
+ChitterSchema.plugin(autoIncrement.plugin, {
+  model: "Chitter",
+  startAt: 0,
+  incrementBy: 1
+});
+module.exports = mongoose.model("Chitter", ChitterSchema);
+const Chit = mongoose.model("Chitter", ChitterSchema);
+
+const tweet = new Chit({
+  userName: "Gary Peabody",
+  content: "Welcome to my first chitter!"
+});
+// tweet.save()
+
+app.get("/", function(req, res) {
+  loadChits();
+  res.sendFile(__dirname + "/index.html");
+});
+
+function loadChits() {
+  Chit.find({}, "content userName")
+    .lean()
+    .sort({ _id: -1 })
+    .exec(function(err, entries) {
+      fs.writeFile("public/allTweets.json","[]",function(err){console.log("blank slate")})
+
+      let currentVal = JSON.stringify(entries);
+      fs.readFile("public/allTweets.json", function(err, filetext) {
+        
+        let chitFile = JSON.parse(filetext);
+        
+        chitFile.push(currentVal);
+        fs.writeFile(
+          "public/allTweets.json",
+          JSON.stringify(chitFile),
+          function(err) {
+            console.log(err);
+            console.log("File Written");
+          }
+        );
+      });
+    });
+}
+
+app.post("/", uploadChit);
+
+function uploadChit(req, res) {
+  const tweet = new Chit({
+    userName: req.body.userName,
+    content: req.body.chitText
+  });
+  tweet.save();
+
+  res.redirect("/");
+  //  res.send("Thank you for your kind words!");
+}
+
+app.listen(3000, function() {
+  console.log("Server up");
+});
+
---
+++ public/index.js
@@ -0,0 +1,42 @@
+$("#chitText_textbox").click(function() {
+  $(this).css("height", "150px");
+});
+
+$.getJSON("/allTweets.json", function(data) {
+  $.each(data, function(key, val) {
+    const ppsmol = JSON.parse(val);
+
+    for (let index = 0; index < 5; index++) {
+      const element = ppsmol[index];
+      console.log(element);
+      const newChit =
+        '<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">' +
+        element.userName +
+        '</h5><p class="card-text">' +
+        element.content +
+        "</p></div></div>";
+
+      $(newChit).insertBefore("#ChitLoader");
+    }
+    let chitCount = 5;
+    $("#ChitLoader").click(function() {
+      console.log(chitCount);
+      console.log(ppsmol.length);
+      if (chitCount >= ppsmol.length - 5) {
+        $(this).hide();
+      }
+      for (let index = chitCount; index < chitCount + 5; index++) {
+        const element = ppsmol[index];
+        const newChit =
+          '<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">' +
+          element.userName +
+          '</h5><p class="card-text">' +
+          element.content +
+          "</p></div></div>";
+
+        $(newChit).insertBefore("#ChitLoader");
+      }
+      chitCount += 5;
+    });
+  });
+});
